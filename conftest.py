d17523aa0dbcf030df326dc9bbbae3628da3385b
Jaco <jacodecristo1999@gmail.com>
2024-02-04 15:16:24 +0100

    add action after tests ended

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+import os
+
+
+def pytest_sessionfinish(session, exitstatus):
+    """ whole test run finishes. """
+    tests_failed = session.testsfailed
+    tests_collected = session.testscollected
+    output = {
+        "tests_failed": tests_failed,
+        "tests_collected": tests_collected,
+        
+    }
+    print("\n",output)
+
